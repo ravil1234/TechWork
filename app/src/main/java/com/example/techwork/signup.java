ee6a034e8fe041c22b6b091d684d7d26fc52cd76
Ravil <mohdravil000@gmail.com>
2020-01-11 02:30:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/techwork/signup.java
@@ -0,0 +1,13 @@
+package com.example.techwork;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
